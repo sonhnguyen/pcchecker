e9f30efa1e061bfc56975bdf4ea50ffb99f91800
sonhnguyen <nhson@apcs.vn>
2016-09-26 23:35:50 +0700

    add

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
+    "github.com/russross/blackfriday"
 )
 
 func main() {
@@ -23,6 +24,10 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
+	router.GET("/mark", func(c *gin.Context) {
+  		c.String(http.StatusOK, string(blackfriday.MarkdownBasic([]byte("**hi!**"))))
+	})
+
 
 	router.Run(":" + port)
 }
