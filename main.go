0fd58a411aa34976265f9567c5f120f944b9f9cc
sonhnguyen <nhson@apcs.vn>
2016-09-29 13:53:14 +0700

    done tan doanh crawler

--- main.go
+++ main.go
@@ -27,19 +27,12 @@ type PcItem struct {
 	Vendor		string
 }
 
-type Items []*PcItem
-func (m Items) Len() int {
-	return len(m)
-}
-
-
-func ScrapeTanDoanh() ([]*PcItem, error) {
+func ScrapeTanDoanh (res []PcItem) ([]PcItem, error) {
 	ROOT_URL := "http://tandoanh.vn"
 	doc, err := goquery.NewDocument(ROOT_URL + "/baogia.php")
 	if err != nil {
 		return nil, err
 	}
-	res := []*PcItem{}
 	doc.Find("div.accordion table tbody tr").Each(func (i int, s *goquery.Selection) {
 		category := s.Closest(".accordion").PrevFiltered("h3").Text()
 		category = category[14:]
@@ -57,95 +50,19 @@ func ScrapeTanDoanh() ([]*PcItem, error) {
 			}
 			col4 := s.Find("td:nth-child(4)").Text()
 			col5 := s.Find("td:nth-child(5)").Text()
-			item := &PcItem{Title: col2, Price: price, Guarantee: col4, Available: col5, Vendor: "tandoanh", Category: category}
+			item := PcItem{Title: col2, Price: price, Guarantee: col4, Available: col5, Vendor: "tandoanh", Category: category}
 			res = append(res, item)
 		}
 	})
 
-	// catPage.Find(".title_pro_home a").Each(func(i int, s *goquery.Selection) {
-	// 	iTitle := s.Text()
-	// 	iLink, exists := s.Attr("href")
-	// 	if !exists {
-	// 		iLink = ""
-	// 	} else {
-	// 		iLink = "http://tandoanh.vn" + iLink 
-	// 	}
-	// 	item := &PcItem{Title: iTitle, Link: iLink, Category: category}
-	// 	res = append(res, item)
-	// })
-	// catPage.Find("tbody tr b font").Each(func(i int, s *goquery.Selection) {
-	// 	item :=res[i]
-	// 	iPrice := s.Text()
-	// 	if(iPrice != "") {
-	// 		iPrice = strings.Replace(iPrice, " ", "", -1)
-	// 		iPrice = strings.Replace(iPrice, ",", "", -1)
-	// 	}
-	// 	item.Price = iPrice
-	// })
-
-	// doc.Find(".athing").Each(func(i int, s *goquery.Selection) {
-	// 	el := s.Find(".title a")
-	// 	// If there's more than one found, reduce it to the first element
-	// 	if el.Size() > 1 {
-	// 		el = el.Slice(0, 1)
-	// 	}
-
-	// 	title := el.Text()
-	// 	if err != nil {
-	// 		fmt.Printf("error grabbing html: %s\n", err)
-	// 		return
-	// 	}
-	// 	link, exists := s.Find(".title a").Attr("href")
-	// 	if !exists {
-	// 		link = ""
-	// 	}
-
-	// 	if strings.HasPrefix(link, "item?") {
-	// 		link = ROOT_URL + "/" + link
-	// 	}
-	// 	item := &NewsItem{Title: title, Link: link}
-	// 	res = append(res, item)
-	// 	//fmt.Printf("%v - %v\n", title, link)
-	// })
-
-	// doc.Find(".subtext").Each(func(i int, s *goquery.Selection) {
-	// 	pString := s.Find(".score").Text()
-	// 	cString := s.Find("a").Last().Text()
-	// 	cLink, exists := s.Find("a").Last().Attr("href")
-	// 	if !exists {
-	// 		cLink = ""
-	// 	} else {
-	// 		cLink = ROOT_URL + "/" + cLink
-	// 	}
-	// 	points := 0
-	// 	comments := 0
-
-	// 	if pString != "" {
-	// 		pSt := strings.Fields(pString)[0]
-	// 		points, err = strconv.Atoi(pSt)
-	// 		if err != nil {
-	// 			points = 0
-	// 		}
-	// 	}
-
-	// 	if cString != "" && cString != "discuss" {
-	// 		cSt := strings.Fields(cString)[0]
-	// 		comments, err = strconv.Atoi(cSt)
-	// 		if err != nil {
-	// 			comments = 0
-	// 		}
-	// 	}
-
-	// 	item := res[i]
-	// 	item.Points = points
-	// 	item.Comments = comments
-	// 	item.CommentsLink = cLink
-	// })
-
 	return res, nil
 }
 
-func insertMlab(items []*PcItem ) {
+func ScrapeHH(res []PcItem) ([]PcItem, error) {
+	res = append(res, PcItem{Title: "hello"})
+	return res, nil
+}
+func insertMlab(items []PcItem ) {
         uri := os.Getenv("MONGODB_URI")
         if uri == "" {
                 fmt.Println("no connection string provided")
@@ -160,13 +77,14 @@ func insertMlab(items []*PcItem ) {
         defer sess.Close()
         sess.SetSafe(&mgo.Safe{})
         collection := sess.DB("heroku_tr3z0r48").C("godata")
+        //remove all before insert
+        collection.RemoveAll(nil)
 
+        //prepare bulk insert
         docs := make([]interface{}, len(items))
 		for i := 0; i < len(items); i++ {
 			docs[i] = items[i]
 		}
-
-
 		x := collection.Bulk()
 		x.Unordered() //magic! :)
 		x.Insert(docs...)
@@ -174,6 +92,7 @@ func insertMlab(items []*PcItem ) {
 		if (err!=nil) {
 			panic(err)
 		}
+
 		fmt.Printf("%v", res)
 }
 
@@ -192,12 +111,21 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
+	pcItems := []PcItem{}
 
-	pcItems, err := ScrapeTanDoanh()
+	pcItems, err := ScrapeTanDoanh(pcItems)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println(err, pcItems)
 	}
-	insertMlab(pcItems)
+	fmt.Printf("length of pcitems 1: %v", len(pcItems))
+
+	pcItems, err = ScrapeHH(pcItems)
+	if err != nil {
+		fmt.Println(err, pcItems)
+	}
+
+	fmt.Printf("length of pcitems 2: %v", len(pcItems))
+	//insertMlab(pcItems)
 	router.Run(":" + port)
 
 }
