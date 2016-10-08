aaebf2fe3d4295ce4f783a9e4fead2939cf1683d
sonhnguyen <nhson@apcs.vn>
2016-10-08 16:02:10 +0700

    something

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -3,6 +3,7 @@ package crawler
 import (
 	"os"
 	"fmt"
+    "gopkg.in/mgo.v2/bson"	
     "gopkg.in/mgo.v2"
 	"github.com/PuerkitoBio/goquery"
 	"strconv"
@@ -302,7 +303,29 @@ func ScrapeAZ(res []PcItem) ([]PcItem, error) {
 	}
 	return res, nil
 }
+func GetMLab() ([]PcItem, error) {
+        uri := os.Getenv("MONGODB_URI")
+        if uri == "" {
+                fmt.Println("no connection string provided")
+                os.Exit(1)
+        }
+ 
+        sess, err := mgo.Dial(uri)
+        if err != nil {
+                fmt.Printf("Can't connect to mongo, go error %v\n", err)
+                os.Exit(1)
+        }
+        defer sess.Close()
+        sess.SetSafe(&mgo.Safe{})
+        collection := sess.DB("heroku_tr3z0r48").C("godata")
 
+        var results []PcItem
+		err = collection.Find(bson.M{"category": "HDD / SSD"}).Sort("-timestamp").All(&results)
+		if err != nil {
+			panic(err)
+		}
+		return results, err
+}
 func InsertMlab(items []PcItem ) {
         uri := os.Getenv("MONGODB_URI")
         if uri == "" {
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/heroku/go-getting-started/crawler"
 )
+var pcItems = []crawler.PcItem{}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -24,15 +25,20 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
-	pcItems := []crawler.PcItem{}
-	pcItems, err:= crawler.ScrapeAZ(pcItems)
-	if err != nil {
-		fmt.Println(err, pcItems)
-	}
-	crawler.InsertMlab(pcItems)
 
-	fmt.Printf("length of pcitems 1: %v", len(pcItems))
-		router.Run(":" + port)
+
+	router.GET("/queryAll", func(c *gin.Context) {
+		pcItems, err:= crawler.GetMLab()
+		fmt.Printf("length of pcitems 1: %v", pcItems)
+
+		if err != nil {
+			fmt.Println(err, pcItems)
+		}
+		c.JSON(200, pcItems)
+
+	})
+
+	router.Run(":" + port)
 
 
 }
