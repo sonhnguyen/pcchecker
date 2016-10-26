41dcbc372104a0d8d347c4364efc90b9606b7905
sonhnguyen <nhson@floatingcube.com>
2016-10-26 14:40:22 +0700

    4

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 		go crawler.Run()
 	})
 	router.GET("/wakemydyno.txt", func(c *gin.Context) {
-		c.JSON(200, "helloooooo")
+		c.JSON(200, "helloooooooooo")
 	})
 	router.GET("/getProducts/:category/", productService.GetProducts)
 	router.GET("/product/:id/", productService.GetProduct)
