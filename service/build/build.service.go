d9abcc5fbaee283ac921e8a08cbe91b42a48cef4
Trung Mai <Trung Mai>
2016-10-30 16:15:17 +0700

    add encode feature for create build

--- service/build/build.service.go
+++ service/build/build.service.go
@@ -2,11 +2,13 @@ package buildService
 
 import (
 	"errors"
-	"fmt"
 	"math"
+	"math/rand"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sonhnguyen/pcchecker/mlabConnector"
 	"github.com/sonhnguyen/pcchecker/service/response"
 )
 
@@ -15,21 +17,26 @@ type CreateBuildPostData struct {
 	So int    `json:"number" binding:"required"`
 }
 
+var buildCollection, _ = mlabConnector.GetCollection("build")
+
 func CreateBuild(c *gin.Context) {
 	var data CreateBuildPostData
 	err := c.BindJSON(&data)
 	if err != nil {
 		c.JSON(400, gin.H{"error": responseService.ResponseError(400, err, "CONNECT_ERROR"), "result": nil})
 	}
-	datetimeNow := time.Now()
-	fmt.Println(encode(datetimeNow.UnixNano() / int64(time.Microsecond)))
-	fmt.Println(datetimeNow.UnixNano() / int64(time.Microsecond))
-	fmt.Println(datetimeNow.UnixNano())
-
+	var datetimeNow time.Time
+	encodedString := ""
+	for len(encodedString) != 11 {
+		datetimeNow = time.Now()
+		randomIntWithDateNow := strconv.Itoa(rand.Intn(1e2)) + strconv.FormatInt(datetimeNow.UnixNano()/int64(time.Microsecond), 10)
+		if n, err := strconv.ParseInt(randomIntWithDateNow, 10, 64); err == nil {
+			encodedString = encode(n)
+		}
+	}
 	c.JSON(200, gin.H{
 		"error":  responseService.ResponseError(200, errors.New("OK"), "OK"),
-		"result": data})
-
+		"result": encodedString})
 }
 
 func encode(num int64) string {
@@ -42,7 +49,6 @@ func encode(num int64) string {
 		if remainder < 0 {
 			remainder += base
 		}
-		fmt.Println(remainder, divisionResult)
 		divisionResult = math.Floor(divisionResult / float64(base))
 		encoded = string(alphabet[remainder]) + encoded
 	}
